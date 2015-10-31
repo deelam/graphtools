99d0c424398b400853d0696e9b9c64aaec310fd2
deelam <dnaelam@gmail.com>
2015-10-30 19:27:40 -0500

    set up maven poms

---
+++ dev/importer/src/main/java/graphtools/Importer.java
@@ -0,0 +1,13 @@
+package graphtools;
+
+import com.google.common.collect.Lists;
+
+/**
+ * 
+ * @author dlam
+ */
+public class Importer {
+	public static void main(String[] args) {
+		Lists.newArrayList();
+	}
+}

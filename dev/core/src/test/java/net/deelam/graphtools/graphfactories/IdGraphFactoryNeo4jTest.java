98aad62f087f14ecf4949b006b32e5067bb7491e
D. Lam <dnaelam@gmail.com>
2015-12-03 14:23:27 -0600

    add test for transactions and working with 2 graphs

--- dev/core/src/test/java/net/deelam/graphtools/graphfactories/IdGraphFactoryNeo4jTest.java
+++ dev/core/src/test/java/net/deelam/graphtools/graphfactories/IdGraphFactoryNeo4jTest.java
@@ -1,14 +1,20 @@
 package net.deelam.graphtools.graphfactories;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 
+import net.deelam.graphtools.GraphTransaction;
 import net.deelam.graphtools.GraphUri;
+import net.deelam.graphtools.GraphUtils;
 
 import org.apache.commons.configuration.BaseConfiguration;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.common.collect.Iterables;
+import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
 import com.tinkerpop.blueprints.util.wrappers.id.IdGraph;
 
@@ -39,7 +45,7 @@ public class IdGraphFactoryNeo4jTest {
     IdGraph<Neo4jGraph> graph = gUri.openIdGraph(Neo4jGraph.class);
     graph.shutdown();
   }
-  
+
   @Test
   public void testNeo4jUsingConf() throws IOException {
     BaseConfiguration conf = new BaseConfiguration();
@@ -61,13 +67,57 @@ public class IdGraphFactoryNeo4jTest {
   }
 
   @Test
-  public void testTwoNeo4jUsingUriQuery() {
+  public void testTwoNeo4jUsingUriQuery() throws IOException {
     GraphUri gUri = new GraphUri("neo4j:./target/myDb?node_auto_indexing=true");
-    IdGraph<Neo4jGraph> graph = gUri.openIdGraph();
-
+    IdGraph<Neo4jGraph> graph = gUri.createNewIdGraph(true);
     GraphUri gUri2 = new GraphUri("neo4j:./target/myDb2?node_auto_indexing=true");
-    IdGraph<Neo4jGraph> graph2 = gUri2.openIdGraph(Neo4jGraph.class);
+    IdGraph<Neo4jGraph> graph2 = gUri2.createNewIdGraph(true);
 
+    int tx = GraphTransaction.begin(graph);
+    Vertex a = null, b = null;
+    try {
+      a = graph.addVertex("A");
+      a.setProperty("prop", "value");
+      b = graph.addVertex("B");
+      graph.addEdge("E", a, b, "edgey");
+      GraphTransaction.commit(tx);
+
+      tx = GraphTransaction.begin(graph2);
+      try {
+        Vertex a2 = graph2.addVertex("A2");
+        a2.setProperty("prop2", "value2");
+        a2.setProperty("prop", a.getProperty("prop"));
+        Vertex b2 = graph2.addVertex("B2");
+
+        try{
+          graph2.addEdge("E", a, b, "edgey2");
+          fail("exception should be thrown since nodes a and b are not in graph2");
+        } catch (RuntimeException re) {
+        }
+        
+        graph2.addEdge("E", a2, b2, "edgey2");
+        GraphTransaction.commit(tx);
+      } catch (RuntimeException re) {
+        GraphTransaction.rollback(tx);
+        throw re;
+      }
+    } catch (RuntimeException re) {
+      GraphTransaction.rollback(tx);
+      throw re;
+    }
+
+    assertEquals(3, Iterables.size(graph.getVertices()));
+    assertEquals(1, Iterables.size(graph.getEdges()));
+    assertEquals(3, Iterables.size(graph2.getVertices()));
+    assertEquals(1, Iterables.size(graph2.getEdges()));
+    assertEquals("edgey", graph.getEdge("E").getLabel());
+    assertEquals("edgey2", graph2.getEdge("E").getLabel());
+    assertEquals("value", graph.getVertex("A").getProperty("prop"));
+    assertEquals("value2", graph2.getVertex("A2").getProperty("prop2"));
+    assertEquals("value",  graph2.getVertex("A2").getProperty("prop"));
+    
+//    System.out.println(GraphUtils.toString(graph));
+//    System.out.println(GraphUtils.toString(graph2));
     graph.shutdown();
     graph2.shutdown();
   }
--- dev/core/src/test/java/net/deelam/graphtools/graphfactories/IdGraphFactoryOrientdbTest.java
+++ dev/core/src/test/java/net/deelam/graphtools/graphfactories/IdGraphFactoryOrientdbTest.java
@@ -1,14 +1,20 @@
 package net.deelam.graphtools.graphfactories;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 
+import net.deelam.graphtools.GraphTransaction;
 import net.deelam.graphtools.GraphUri;
+import net.deelam.graphtools.GraphUtils;
 
 import org.apache.commons.configuration.BaseConfiguration;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.common.collect.Iterables;
+import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.util.wrappers.id.IdGraph;
 
@@ -84,13 +90,59 @@ public class IdGraphFactoryOrientdbTest {
   }
 
   @Test
-  public void testTwoOrientUsingUriQuery() {
+  public void testTwoOrientUsingUriQuery() throws IOException {
     GraphUri gUri = new GraphUri("orientdb:plocal:./target/myODb");
-    IdGraph<OrientGraph> graph = gUri.openIdGraph();
-
+    IdGraph<OrientGraph> graph = gUri.createNewIdGraph(true);
     GraphUri gUri2 = new GraphUri("orientdb:plocal:./target/myODb2");
-    IdGraph<OrientGraph> graph2 = gUri2.openIdGraph();
+    IdGraph<OrientGraph> graph2 = gUri2.createNewIdGraph(true);
 
+    int tx = GraphTransaction.begin(graph);
+    Vertex a=null,b=null;
+    try {
+      a = graph.addVertex("A");
+      a.setProperty("prop", "value");
+      b = graph.addVertex("B");
+      graph.addEdge("E", a, b, "edgey");
+      GraphTransaction.commit(tx);
+
+    tx = GraphTransaction.begin(graph2);
+      try {
+        Vertex a2 = graph2.addVertex("A2");
+        a2.setProperty("prop2", "value2");
+        a2.setProperty("prop", a.getProperty("prop"));
+        Vertex b2 = graph2.addVertex("B2");
+
+        try {
+          graph2.addEdge("E", a, b, "edgey2");
+          GraphTransaction.commit(tx);
+          fail("exception should be thrown since nodes a and b are not in graph2");
+        } catch (RuntimeException re) {
+        }
+        
+        graph2.addEdge("E", a2, b2, "edgey2");
+        GraphTransaction.commit(tx);
+
+      } catch (RuntimeException re) {
+        GraphTransaction.rollback(tx);
+        throw re;
+      }
+    } catch (RuntimeException re) {
+      GraphTransaction.rollback(tx);
+      throw re;
+    }
+    
+    assertEquals(3, Iterables.size(graph.getVertices()));
+    assertEquals(1, Iterables.size(graph.getEdges()));
+    assertEquals(3, Iterables.size(graph2.getVertices()));
+    assertEquals(1, Iterables.size(graph2.getEdges()));
+    assertEquals("edgey", graph.getEdge("E").getLabel());
+    assertEquals("edgey2", graph2.getEdge("E").getLabel());
+    assertEquals("value", graph.getVertex("A").getProperty("prop"));
+    assertEquals("value2", graph2.getVertex("A2").getProperty("prop2"));
+    assertEquals("value",  graph2.getVertex("A2").getProperty("prop"));
+
+//    System.out.println(GraphUtils.toString(graph));
+//    System.out.println(GraphUtils.toString(graph2));
     graph.shutdown();
     graph2.shutdown();
   }

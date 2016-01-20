f86838b69e00f87b11abef2f63fb12d0fdbe311e
D. Lam <dnaelam@gmail.com>
2016-01-20 17:51:22 -0600

    add onDisk indexer test

--- dev/enricher/src/test/java/net/deelam/enricher/indexing/NodeIndexerTest.java
+++ dev/enricher/src/test/java/net/deelam/enricher/indexing/NodeIndexerTest.java
@@ -1,6 +1,8 @@
 package net.deelam.enricher.indexing;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import net.deelam.enricher.indexing.domain.LocationIndexer;
 import net.deelam.enricher.indexing.domain.PersonIndexer;
@@ -8,7 +10,9 @@ import net.deelam.graphtools.GraphUri;
 import net.deelam.graphtools.graphfactories.IdGraphFactoryTinker;
 
 import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.io.FileUtils;
 import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
 import org.junit.After;
 import org.junit.Before;
@@ -38,10 +42,23 @@ public class NodeIndexerTest {
   }
 
   @Test
-  public void test() throws IOException, ConfigurationException, ParseException {
-//    FileUtils.deleteDirectory(new File("target/us500index"));
-    
+  public void testInMemory() throws IOException, ConfigurationException, ParseException {
     try(NodeIndexer indexer = new NodeIndexer(new RAMDirectory())){
+      indexer.registerEntityIndexer(new PersonIndexer());
+      indexer.registerEntityIndexer(new LocationIndexer());
+      
+      indexer.indexGraph(graph, "us500test");
+      indexer.indexGraph(graph, "us500test2");
+      indexer.listBySortedField("firstNameSorted", "STRING", "firstName", 20);
+      indexer.listBySortedField("zipSorted", "INT", "zip",20 );
+    }
+  }
+  
+  @Test
+  public void testOnDisk() throws IOException, ConfigurationException, ParseException {
+    FileUtils.deleteDirectory(new File("target/nodeIndxr"));
+    String path = "target/nodeIndxr";
+    try(NodeIndexer indexer = new NodeIndexer(FSDirectory.open(Paths.get(path)))){
       indexer.registerEntityIndexer(new PersonIndexer());
       indexer.registerEntityIndexer(new LocationIndexer());
       

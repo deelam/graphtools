82dfb88553787fd1d0814d0bcc486a73c91d1423
deelam <dnaelam@gmail.com>
2016-01-23 21:19:56 -0600

    convert non-primitive property values

--- dev/importer/src/main/java/net/deelam/graphtools/importer/GraphRecordBuilder.java
+++ dev/importer/src/main/java/net/deelam/graphtools/importer/GraphRecordBuilder.java
@@ -1,18 +1,26 @@
 package net.deelam.graphtools.importer;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.deelam.graphtools.GraphRecord;
 import net.deelam.graphtools.GraphRecordEdge;
 
 import com.google.common.base.Preconditions;
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Element;
 import com.tinkerpop.blueprints.util.wrappers.id.IdGraph;
 
 /**
+ * Not thread-safe
  * @author deelam, Created:Nov 10, 2015
  */
 @RequiredArgsConstructor
@@ -95,8 +103,64 @@ public class GraphRecordBuilder<B> {
         }
       }
     }
-
-    return gRecords.values();
+    Collection<GraphRecord> records = gRecords.values();
+    convertPropertyValues(records);
+    return records;
   }
 
+  @Getter
+  private final static Set<Class<?>> validPropertyClasses = new HashSet<>();
+  {
+    validPropertyClasses.add(String.class);
+    validPropertyClasses.add(Integer.class);
+    validPropertyClasses.add(Boolean.class);
+    validPropertyClasses.add(Float.class);
+    validPropertyClasses.add(Long.class);
+    validPropertyClasses.add(Double.class);
+    validPropertyClasses.add(Byte.class);
+    validPropertyClasses.add(Character.class);
+    validPropertyClasses.add(Short.class);
+  }
+  
+  private void convertPropertyValues(Collection<GraphRecord> records) {
+    for(GraphRecord rec:records){
+      convertProperties(rec);
+      for(Edge e:rec.getInEdges().values()){
+        convertProperties(e);
+      }
+      for(Edge e:rec.getOutEdges().values()){
+        convertProperties(e);
+      }
+    }
+  }
+
+  Collection<String> keys=new ArrayList<>();
+  private void convertProperties(Element elem) {
+    keys.clear();
+    for(String key:elem.getPropertyKeys()){
+      Object val = elem.getProperty(key);
+      if(val!=null && !validPropertyClasses.contains(val.getClass())){
+        if(val instanceof Date){
+          keys.add(key);
+        } else {
+          log.warn("Unhandled property value of class="+val.getClass()+" "+val);
+        }
+      }
+    }
+    for(String key:keys){
+      Object val = elem.getProperty(key);
+      elem.setProperty(key, ((Date)val).getTime()); // use this to deserialize
+      elem.setProperty(key+"_string", val.toString()); // this is for readability
+    }
+  }
+
+  public static void main(String[] args) {
+    Object i=1d;
+    System.out.println(i.getClass());
+    System.out.println((i instanceof String));
+    System.out.println((i instanceof Integer));
+    System.out.println((i instanceof Long));
+    System.out.println((i instanceof Float));
+    System.out.println((i instanceof Boolean));
+  }
 }

4732508de606fcc8b8c90fdf108e21d6f0517497
D. Lam <dnaelam@gmail.com>
2016-03-11 09:49:49 -0600

    add FramedGraphProvider support for PartitionGraph

--- dev/core/src/main/java/net/deelam/graphtools/FramedGraphProvider.java
+++ dev/core/src/main/java/net/deelam/graphtools/FramedGraphProvider.java
@@ -11,6 +11,7 @@ import com.tinkerpop.blueprints.TransactionalGraph;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 import com.tinkerpop.blueprints.util.wrappers.WrapperGraph;
+import com.tinkerpop.blueprints.util.wrappers.partition.PartitionGraph;
 import com.tinkerpop.blueprints.util.wrappers.readonly.ReadOnlyGraph;
 import com.tinkerpop.frames.FramedGraph;
 import com.tinkerpop.frames.FramedGraphFactory;
@@ -27,116 +28,117 @@ import com.tinkerpop.frames.modules.typedgraph.TypedGraphModuleBuilder;
 @Slf4j
 public class FramedGraphProvider {
 
-	private final FramedGraphFactory factory;
-	
-	private Map<String,Class<?>> frameClassesMap=new HashMap<>();
+  private final FramedGraphFactory factory;
 
-    public FramedGraphProvider(Class<?>[] typedClasses){
-      this(null, typedClasses);
+  private Map<String, Class<?>> frameClassesMap = new HashMap<>();
+
+  public FramedGraphProvider(Class<?>[] typedClasses) {
+    this(null, typedClasses);
+  }
+
+  public FramedGraphProvider(AbstractModule initializersModule,
+      Class<?>[] typedClasses) {
+    TypedGraphModuleBuilder tgmb = new TypedGraphModuleBuilder();
+    for (Class<?> clazz : typedClasses) {
+      tgmb = tgmb.withClass(clazz);
+      registerFrameClass(clazz);
     }
-    
-	public FramedGraphProvider(AbstractModule initializersModule,
-		Class<?>[] typedClasses){
-		TypedGraphModuleBuilder tgmb=new TypedGraphModuleBuilder();
-		for(Class<?> clazz:typedClasses){
-			tgmb=tgmb.withClass(clazz);
-			registerFrameClass(clazz);
-		}
 
-		log.debug("Registered Frame class types: {}", frameClassesMap.keySet());
+    log.debug("Registered Frame class types: {}", frameClassesMap.keySet());
 
-		if(initializersModule==null){
-			factory=new FramedGraphFactory(
-				new JavaHandlerModule(), // required to activate @JavaHandler annotations
-				tgmb.build() // required to store type info
-				);
-		} else {
-			factory=new FramedGraphFactory(
-				new JavaHandlerModule(), // required to activate @JavaHandler annotations
-				initializersModule,
-				tgmb.build() // required to store type info
-				);
-		}
-	}
+    if (initializersModule == null) {
+      factory = new FramedGraphFactory(
+          new JavaHandlerModule(), // required to activate @JavaHandler annotations
+          tgmb.build() // required to store type info
+          );
+    } else {
+      factory = new FramedGraphFactory(
+          new JavaHandlerModule(), // required to activate @JavaHandler annotations
+          initializersModule,
+          tgmb.build() // required to store type info
+          );
+    }
+  }
 
-//	public boolean containsFrameType(String type){
-//		return frameClassesMap.containsKey(type);
-//	}
-	
-	public Class<?> getFrameClassOf(Vertex v, String typePropKey){
-		final String vertexType=v.getProperty(typePropKey);
-		if(vertexType==null){
-			log.error("getVertexType() returned null.  Does Frame class for vertex {} have a @TypeValue annotation?", v);
-		}
-		
-		final Class<?> ret=frameClassesMap.get(vertexType);
-		if(ret==null){
-			log.error("Could not find class for "+v+".  Has it been registered in *FrameRegistry?");
-		}
-		return ret;
-	}
-	
-	public Class<?> getFrameClassOf(VertexFrame v, String typePropKey){
-		return getFrameClassOf(v.asVertex(), typePropKey);
-	}
-	
-	private void registerFrameClass(Class<?> clazz){
-		try{
-			String typeStr=(String) clazz.getField("TYPE_VALUE").get(clazz);
-			frameClassesMap.put(typeStr, clazz);
-		}catch(IllegalArgumentException e){
-			e.printStackTrace();
-		}catch(SecurityException e){
-			e.printStackTrace();
-		}catch(IllegalAccessException e){
-			e.printStackTrace();
-		}catch(NoSuchFieldException e){
-			e.printStackTrace();
-		}
-	}
+  //	public boolean containsFrameType(String type){
+  //		return frameClassesMap.containsKey(type);
+  //	}
 
-	/**
-	 * Using the other get() methods that return a FramedTransactionalGraph is preferred.
-	 */
-	public <T extends Graph> FramedGraph<T> get(T graph){
-		Preconditions.checkNotNull(graph);
-		// acceptable types are those that do not require committing graph operations
-		if(graph instanceof ReadOnlyGraph){ //acceptable since no graph operations will be performed
-			// return a non-transactional graph 
-		}else if(isGraphType(TinkerGraph.class, graph)){
-			// don't worry about returning a TransactionFramedGraph
-		}else if(isGraphType(TransactionalGraph.class, graph)){
-			throw new IllegalArgumentException("Graph is transactional but unhandled: " + graph);
-		}else{
-			log.warn("Should add {} to the list of acceptable graph types.", graph.getClass().getSimpleName());
-		}
+  public Class<?> getFrameClassOf(Vertex v, String typePropKey) {
+    final String vertexType = v.getProperty(typePropKey);
+    if (vertexType == null) {
+      log.error("getVertexType() returned null.  Does Frame class for vertex {} have a @TypeValue annotation?", v);
+    }
 
-		FramedGraph<T> framedGraph=factory.create(graph); // wrap the base graph
-		return framedGraph;
-	}
+    final Class<?> ret = frameClassesMap.get(vertexType);
+    if (ret == null) {
+      log.error("Could not find class for " + v + ".  Has it been registered in *FrameRegistry?");
+    }
+    return ret;
+  }
 
-	//	public FramedTransactionalGraph<TransactionalPartitionGraph> get(PartitionGraph<? extends TransactionalGraph> graph){
-	//		// Example: Given a partitiongraph[eventtransactionalindexablegraph[idgraph[...]]]
-	//		FramedTransactionalGraph<TransactionalPartitionGraph> framedGraph=factory.create(
-	//			new TransactionalPartitionGraph(graph));
-	//		return framedGraph;
-	//	}
+  public Class<?> getFrameClassOf(VertexFrame v, String typePropKey) {
+    return getFrameClassOf(v.asVertex(), typePropKey);
+  }
 
-	public FramedTransactionalGraph<TransactionalGraph> get(TransactionalGraph graph){
-		FramedTransactionalGraph<TransactionalGraph> framedGraph=factory.create(graph); // wrap the base graph
-		return framedGraph;
-	}
+  private void registerFrameClass(Class<?> clazz) {
+    try {
+      String typeStr = (String) clazz.getField("TYPE_VALUE").get(clazz);
+      frameClassesMap.put(typeStr, clazz);
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    } catch (SecurityException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (NoSuchFieldException e) {
+      e.printStackTrace();
+    }
+  }
 
-	public static boolean isGraphType(Class<?> clazz, Graph g){
-		if(clazz.isInstance(g))
-			return true;
+  /**
+   * Using the other get() methods that return a FramedTransactionalGraph is preferred.
+   */
+  public <T extends Graph> FramedGraph<T> get(T graph) {
+    Preconditions.checkNotNull(graph);
+    // acceptable types are those that do not require committing graph operations
+    if (graph instanceof ReadOnlyGraph) { //acceptable since no graph operations will be performed
+      // return a non-transactional graph 
+    } else if (isGraphType(TinkerGraph.class, graph)) {
+      // don't worry about returning a TransactionFramedGraph
+    } else if (isGraphType(PartitionGraph.class, graph)) {
+    } else if (isGraphType(TransactionalGraph.class, graph)) {
+      throw new IllegalArgumentException("Graph is transactional but unhandled: " + graph);
+    } else {
+      log.warn("Should add {} to the list of acceptable graph types.", graph.getClass().getSimpleName());
+    }
 
-		while(g instanceof WrapperGraph){
-			g= ((WrapperGraph<?>) g).getBaseGraph();
-			if(clazz.isInstance(g))
-				return true;
-		}
-		return false;
-	}
+    FramedGraph<T> framedGraph = factory.create(graph); // wrap the base graph
+    return framedGraph;
+  }
+
+  //	public FramedTransactionalGraph<TransactionalPartitionGraph> get(PartitionGraph<? extends TransactionalGraph> graph){
+  //		// Example: Given a partitiongraph[eventtransactionalindexablegraph[idgraph[...]]]
+  //		FramedTransactionalGraph<TransactionalPartitionGraph> framedGraph=factory.create(
+  //			new TransactionalPartitionGraph(graph));
+  //		return framedGraph;
+  //	}
+
+  public FramedTransactionalGraph<TransactionalGraph> get(TransactionalGraph graph) {
+    FramedTransactionalGraph<TransactionalGraph> framedGraph = factory.create(graph); // wrap the base graph
+    return framedGraph;
+  }
+
+  public static boolean isGraphType(Class<?> clazz, Graph g) {
+    if (clazz.isInstance(g))
+      return true;
+
+    while (g instanceof WrapperGraph) {
+      g = ((WrapperGraph<?>) g).getBaseGraph();
+      if (clazz.isInstance(g))
+        return true;
+    }
+    return false;
+  }
 
 }
